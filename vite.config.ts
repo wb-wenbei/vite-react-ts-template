c1cb7f908a2d462fc5bf99c3f43af938133fd9c4
wenbei <bei.wen@extour-inc.com>
2025-05-13 17:45:59 +0800

    feat: update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,22 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 8000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
